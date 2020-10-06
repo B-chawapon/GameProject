db2f80607739699cfe3568cdd46e4aa762bab66e
Chawapon <chawapon2001@gmail.com>
2020-10-07 00:19:26 +0700

    sprite pass

--- Project3SFML/Project3SFML/texture.cpp
+++ Project3SFML/Project3SFML/texture.cpp
@@ -9,89 +9,119 @@ int main()
 	sf::Texture texturebg;
 	sf::Texture playerTexture;
 
+	
+
 	sf::View view;
 	view.reset(sf::FloatRect(0, 0, screen.x,screen.y));
 	view.setViewport(sf::FloatRect(0, 0, 1.0f, 1.0f));
-	sf::Vector2f position(screen.x/2, screen.y/2);
+	sf::Vector2f positionbg(screen.x/2, screen.y/2);
 	
 
 	texturebg.loadFromFile("starbg.png");
-	playerTexture.loadFromFile("pls.png");
+	playerTexture.loadFromFile("blackmage.png");
 	wizard.setTexture(&playerTexture);
 
 	sf::Sprite sptexturebg;
 	sptexturebg.setTexture(texturebg);
 	sptexturebg.setPosition(0, 0);
-	sptexturebg.setScale({ 1.0f,1.0f });
+	sptexturebg.setScale({ 1.5f,1.5f });
 
-
+	int spriteSizeX = playerTexture.getSize().x / 5;
+	int spriteSizeY = playerTexture.getSize().y / 6;
 	
 	sf::Sprite wizardsp;
 	wizardsp.setTexture(playerTexture);
-	wizardsp.setTextureRect(sf::IntRect(40, 50, 300, 400));
-	wizardsp.setScale({ 0.1, 0.1 });
+	wizardsp.setTextureRect(sf::IntRect(0, 0, spriteSizeX, spriteSizeY));
+	wizardsp.setScale({ 0.8f, 0.8f });
 
-
+	int temp = 1;
+	int animationx = 0;
+	int animationy = 0;
 	
 	while (window.isOpen())
 	{
+		
+		view.setCenter(positionbg);
+		window.setView(view);
+		
+		
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
+		{
+			wizardsp.move(.2f, 0.f);
+			wizardsp.setTextureRect(sf::IntRect(spriteSizeX * animationx, spriteSizeY * (3+animationy), spriteSizeX, spriteSizeY));
+		
+		}
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))
+		{
+			wizardsp.move(-.2f, 0.f);
+			wizardsp.setTextureRect(sf::IntRect(spriteSizeX * animationx, spriteSizeY * animationy, spriteSizeX, spriteSizeY));
+			
+		}
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::W))
+		{
+			wizardsp.move(0.f, -.2f);
+			wizardsp.setTextureRect(sf::IntRect(spriteSizeX * animationx, spriteSizeY * animationy, spriteSizeX, spriteSizeY));
+		}
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::S))
+		{
+			wizardsp.move(0.f, .2f);
+			wizardsp.setTextureRect(sf::IntRect(spriteSizeX * animationx, spriteSizeY * (3 + animationy), spriteSizeX, spriteSizeY));
+		}
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::LControl))
+		{
+			wizardsp.setTextureRect(sf::IntRect(70, 104, 35, 52));
+
+		}
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
+		{
+			window.close();
+		}
+		animationx++;
+		if (animationx >= 5) {
+			temp++;
+			animationx = 0;
+			animationy = 1;
+			if (temp == 3)
+			{
+				animationy = 0;
+				temp = 1;
+			}
+			
+		}
+
+
+		if (wizardsp.getPosition().x+10>screen.x/2)
+		{
+			positionbg.x = wizardsp.getPosition().x+10;
+		}
+		else {
+			positionbg.x = screen.x/2;
+		}
+		
+		if (wizardsp.getPosition().y + 10 > screen.y/2 )
+		{
+			positionbg.y = wizardsp.getPosition().y + 10;
+		}
+		else {
+			positionbg.y = screen.y / 2;
+		}
+	
+
+		
 		sf::Event windowEvent;
 		while (window.pollEvent(windowEvent))
 		{
 			if (windowEvent.type == sf::Event::Closed) {
 				window.close();
 			}
-		}
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
-		{
-			wizardsp.move(.2f, 0.f);
-		}
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))
-		{
-			wizardsp.move(-.2f, 0.f);
-		}
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::W))
-		{
-			wizardsp.move(0.f, -.2f);
-		}
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::S))
-		{
-			wizardsp.move(0.f, .2f);
-		}
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
-		{
-			window.close();
-		}
 
-
-		if (wizardsp.getPosition().x+10>screen.x/2)
-		{
-			position.x = wizardsp.getPosition().x+10;
 		}
-		else {
-			position.x = screen.x/2;
-		}
-		if (wizardsp.getPosition().y + 10 > screen.y / 2)
-		{
-			position.y = wizardsp.getPosition().y + 10;
-		}
-		else {
-			position.y = screen.y / 2;
-		}
-
-		view.setCenter(position);
-		
-		window.setView(view);
+	
 		window.draw(sptexturebg);
-		
-		
-		
-		
 		window.draw(wizardsp);
 		window.display();
+	
 		window.clear();
-		
-		
 	}
 	return 0;
 }

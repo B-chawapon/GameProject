16f36359a060400d01591cddf1f4af54f091d531
Chawapon <chawapon2001@gmail.com>
2020-10-06 12:53:27 +0700

    bg can move

--- Project3SFML/Project3SFML/texture.cpp
+++ Project3SFML/Project3SFML/texture.cpp
@@ -3,22 +3,45 @@
 
 int main()
 {
-	sf::RenderWindow window(sf::VideoMode(1080, 720), "GAME",sf::Style::Close);
+	sf::Vector2i screen(1080, 720);
+	sf::RenderWindow window(sf::VideoMode(screen.x,screen.y), "GAME",sf::Style::Close);
 	sf::RectangleShape wizard(sf::Vector2f(50.0f, 70.0f));
+	sf::Texture texturebg;
 	sf::Texture playerTexture;
 
+	sf::View view;
+	view.reset(sf::FloatRect(0, 0, screen.x,screen.y));
+	view.setViewport(sf::FloatRect(0, 0, 1.0f, 1.0f));
+	sf::Vector2f position(screen.x/2, screen.y/2);
+	
+
+	texturebg.loadFromFile("starbg.png");
 	playerTexture.loadFromFile("pls.png");
 	wizard.setTexture(&playerTexture);
 
+	sf::Sprite sptexturebg;
+	sptexturebg.setTexture(texturebg);
+	sptexturebg.setPosition(0, 0);
+	sptexturebg.setScale({ 1.0f,1.0f });
+
+
+	
 	sf::Sprite wizardsp;
 	wizardsp.setTexture(playerTexture);
 	wizardsp.setTextureRect(sf::IntRect(40, 50, 300, 400));
 	wizardsp.setScale({ 0.1, 0.1 });
+
+
 	
 	while (window.isOpen())
 	{
-		
-		
+		sf::Event windowEvent;
+		while (window.pollEvent(windowEvent))
+		{
+			if (windowEvent.type == sf::Event::Closed) {
+				window.close();
+			}
+		}
 		if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
 		{
 			wizardsp.move(.2f, 0.f);
@@ -39,16 +62,34 @@ int main()
 		{
 			window.close();
 		}
-		sf::Event cat;
-		while (window.pollEvent(cat)) 
-{
-			if (cat.type == cat.Closed) {
-				window.close();
-			}
+
+
+		if (wizardsp.getPosition().x+10>screen.x/2)
+		{
+			position.x = wizardsp.getPosition().x+10;
 		}
-		window.clear();
+		else {
+			position.x = screen.x/2;
+		}
+		if (wizardsp.getPosition().y + 10 > screen.y / 2)
+		{
+			position.y = wizardsp.getPosition().y + 10;
+		}
+		else {
+			position.y = screen.y / 2;
+		}
+
+		view.setCenter(position);
+		
+		window.setView(view);
+		window.draw(sptexturebg);
+		
+		
+		
+		
 		window.draw(wizardsp);
 		window.display();
+		window.clear();
 		
 		
 	}
